ad48b685dd700f6aca0ec0a5c15a692bfb1c06bf
Senpai <109900504+DaniiL10062005@users.noreply.github.com>
2025-02-12 17:37:56 +0300

    ref: Добавлена карточка темной темы, добавлена theme в сторадж #DEV-15 (#12)

--- shared/context/chopp-global-context.tsx
+++ shared/context/chopp-global-context.tsx
@@ -2,16 +2,12 @@ import { PropsWithChildren } from "react";
 import { AuthProvider } from "./auth-context";
 import { ChoppThemeProvider } from "./chopp-theme-context";
 import { ChoppSnackbarStack } from "../components/chopp-snackbar-stack";
-import { ChoppDevBar } from "../components/chopp-dev-bar";
 
-export const ChoppGlobalProvider = ({
-  children,
-}: PropsWithChildren<object>) => {
+export const ChoppGlobalProvider = ({ children }: PropsWithChildren<object>) => {
   return (
     <AuthProvider>
       <ChoppThemeProvider>
         <ChoppSnackbarStack>{children}</ChoppSnackbarStack>
-        <ChoppDevBar />
       </ChoppThemeProvider>
     </AuthProvider>
   );
